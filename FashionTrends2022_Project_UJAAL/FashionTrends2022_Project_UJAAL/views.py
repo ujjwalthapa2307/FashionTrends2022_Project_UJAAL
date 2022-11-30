bd8c9e6abe4fed85843643bceeea7a18362db12d
unknown <ujaal.conestoga@gmail.com>
2022-11-30 14:30:30 -0500

    Login Page Done

--- FashionTrends2022_Project_UJAAL/FashionTrends2022_Project_UJAAL/views.py
+++ FashionTrends2022_Project_UJAAL/FashionTrends2022_Project_UJAAL/views.py
@@ -1,12 +1,41 @@
 """
 Routes and views for the flask application.
 """
-
-from datetime import datetime
-from flask import render_template
+from datetime import datetime        
+from flask import Flask,session,render_template,request,redirect,url_for,flash
+import pyodbc  
 from FashionTrends2022_Project_UJAAL import app
 
+app.secret_key='asdsdfsdfs13sdf_df%&'
+# creating connection Object which will contain SQL Server Connection    
+connection = pyodbc.connect('Driver={SQL Server};Server=.;Database=Fashion_Trends_2022;uid=sa;pwd=Test@1234')# Creating Cursor    
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']  
+        res = connection.execute("SELECT * FROM User_login WHERE Email ='"+username+"'"+"AND Password ='"+ password+"'").fetchone()
+        if res is None:
+            flash("no email found","danger")
+            return render_template('login.html')
+        else:
+            return redirect(url_for('home'))
+        connection.close() 
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username',None)
+    return redirect(url_for('index'))
+
 @app.route('/')
+def index():
+    login=False
+    if 'username' in session:
+        login=True
+    return render_template('login_home.html',login=login)
+
 @app.route('/home')
 def home():
     """Renders the home page."""
